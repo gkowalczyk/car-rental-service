a85359c3526aa3043b83ab2224d1982ef00b4ba8
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2022-06-20 17:11:33 +0200

    dodany scheduler

---
+++ src/main/java/com/example/carrental/config/AdminConfig.java
@@ -0,0 +1,16 @@
+package com.example.carrental.config;
+
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class AdminConfig {
+
+    @Value("${admin.mail}")
+    private String adminMail;
+    @Value("${admin.name}")
+    private String adminName;
+   }
